483370179a9c45f9938eca7de122734b728a1d10
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-08-01 08:24:17 +0530

    check sum number

--- src/SumNumber.java
+++ src/SumNumber.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
 
 public class SumNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		while (true) {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Enter your number");
+			int number = scan.nextInt();
+
+			int sum = 0;
+			while(number != 0) {
+				sum = sum + number%10;
+				number =number / 10;
+			}
+			System.out.println("Sum is "+ sum);
+
+		}
+	
+	}
+	
+
 
 	}
 
