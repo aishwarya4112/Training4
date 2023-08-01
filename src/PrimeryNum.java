8a448263346c55b2b300f04a98f4d84139e795be
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-08-01 08:42:57 +0530

    check primery number

---
+++ src/PrimeryNum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PrimeryNum {
+
+	public static void main(String[] args) {
+Scanner scan =new Scanner(System.in);
+		
+		System.out.println("Enter your Number:");
+		int in = scan.nextInt();
+		if(in > 1) {
+		System.out.println("Number is primery");
+
+	}else{
+		System.out.println("Number is odd");
+	}
+	}
+
+
+	}
+
+
