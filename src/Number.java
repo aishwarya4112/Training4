f619fd07823f7e66ab64d32f7fa10bdebf16bf88
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-08-02 12:09:25 +0530

    To check Square,cube and fourth power

---
+++ src/Number.java
@@ -0,0 +1,13 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		
+		int i = 3;
+		System.out.println((i*i));
+		System.out.println(i*i*i);
+		System.out.println(i*i*i*i);
+		}
+	}
+
+
