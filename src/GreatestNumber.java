ba26eb3e21d0016fa23d5adb124beb7c23fe63b0
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-08-02 17:06:20 +0530

    To check Maximum Number

---
+++ src/GreatestNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class GreatestNumber {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your Number:");
+		int in = scan.nextInt();
+		int i = 10;
+		int j = 34;
+		int k = 12;
+		if(i<j && i<k) {
+			System.out.println("maximum number");
+		}else {
+			System.out.println("Smallest number");
+		}
+		}
+
+	}
+
