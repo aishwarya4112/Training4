094aa50f710e04cb70f6907cd8de965b86cb6450
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-08-10 20:07:10 +0530

    Static method parameterised

--- src/method/method.java
+++ src/method/method.java
@@ -22,14 +22,9 @@ public class method {
 			}
 	
 
-		public int sumOfValuse(int number) {
-			int sum = 0;
-			while(number !=0) {
-		sum = sum + number%10;
-		number = number / 10;
-	}
-	return sum;
-	
+		
 }	
 
-}
+
+
+
