72062e169ddc957e31c6b2319efdf45fb94318d1
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-08-14 09:45:21 +0530

    square, cube, forthpower using scanner.

--- src/Number.java
+++ src/Number.java
@@ -1,13 +1,24 @@
-
+import java.util.Scanner;
 public class Number {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("enter a Number: ");
+		 double num = input.nextDouble();
+		double number ;
+		double squareNum ;
+		double cubeNum ;
+		double forthNum ;
+	
+			squareNum = num * num;
+			cubeNum = squareNum * num;
+			forthNum = cubeNum *num;
+			
 		
-		int i = 3;
-		System.out.println((i*i));
-		System.out.println(i*i*i);
-		System.out.println(i*i*i*i);
-		}
+		System.out.println(squareNum);
+		System.out.println(cubeNum);
+		System.out.println(forthNum);
+		
 	}
 
-
+	}
