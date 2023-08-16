4a61fbb95f5b15125b0ac3d3d56bb9b38510def7
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-08-16 17:01:04 +0530

    Maximum Number

--- src/MaximumNum.java
+++ src/MaximumNum.java
@@ -1,27 +1,34 @@
-
+import java.util.Scanner;
+import java.util.Scanner;
 public class MaximumNum {
 
 	public static void main(String[] args) {
 	
-			int A = 10;
-			int B = 34;
-			int C = 12;
+			  Scanner in = new Scanner(System.in);
+			   
+			  System.out.print("Enter 1st number:");
+			  int A = in.nextInt();
+			   
+			  System.out.print("Enter 2nd number:");
+			  int B = in.nextInt();
+			   
+			  System.out.print("Enter 3rd number:");
+			  int C = in.nextInt();
+			   
+			   
+			  if (A > B)
+			   if (A >C)
+			    System.out.println("Maximum num is"+A);
+			   
+			  if (B > A)
+			   if (B >C)
+			    System.out.println("Maximum num is"+B);
+			   
+			  if (C >A)
+			   if (C > B)
+			    System.out.println("Maximum num is"+C);
+			 }
+			}
 			
-			if(A>B) {
-				if(A>C)
-				System.out.println(A + "is maximum num");
-			else 
-				System.out.println(C + "is maximum num");
-			}else {
-				if(B>C)
-				System.out.println(B + "is maximum num");	
-			else
-				System.out.println(C + "is maximum num");
-	}
-				
-		}
-	
-
-	}
 
 
