3c3e40211eb251b55d22c19fc1b64e9c83159231
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-08-16 10:52:40 +0530

    maximum number

---
+++ src/MaximumNum.java
@@ -0,0 +1,27 @@
+
+public class MaximumNum {
+
+	public static void main(String[] args) {
+	
+			int A = 10;
+			int B = 34;
+			int C = 12;
+			
+			if(A>B) {
+				if(A>C)
+				System.out.println(A + "is maximum num");
+			else 
+				System.out.println(C + "is maximum num");
+			}else {
+				if(B>C)
+				System.out.println(B + "is maximum num");	
+			else
+				System.out.println(C + "is maximum num");
+	}
+				
+		}
+	
+
+	}
+
+
