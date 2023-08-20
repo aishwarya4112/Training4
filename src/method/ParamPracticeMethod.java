6e1519437aacda975fb96aeedc23507f80058551
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-08-20 12:41:07 +0530

    Parameterized methos using static non-static method

---
+++ src/method/ParamPracticeMethod.java
@@ -0,0 +1,28 @@
+package method;
+
+public class ParameterizedMethod {
+	 	int sub() {
+		 int i = 40;
+		 int j = 15;
+		 int res = i -j;
+		 return res;
+	}
+
+	   static void add() {
+		  int a = 20;
+		  int b = 40;
+		  int res = a + b;
+		 System.out.println("add =" + res);
+			
+	}
+	  	
+		public static void main(String[] args) {
+				ParameterizedMethod d1 = new ParameterizedMethod();
+				int value = d1.sub();
+				System.out.println("Sub =" + value);
+				ParameterizedMethod.add();
+			}
+	
+	}
+
+
