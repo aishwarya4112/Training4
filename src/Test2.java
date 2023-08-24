d9ae8b94e28c601265f6c5e2d671066432db9d04
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-08-24 19:12:15 +0530

    Swap values using third variable

---
+++ src/Test2.java
@@ -0,0 +1,23 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		int i = 10;
+
+	    int j = 7;
+	    
+	    int temp = i;
+	    i = j;
+	    j = temp;
+	   
+	    System.out.println("value of i is "+i+" value of j is "+j);
+
+	    System.out.println(i);
+
+	    System.out.println(j);
+
+	   
+
+	}
+
+}
