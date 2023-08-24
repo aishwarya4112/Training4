c80e490f5d7f384ba943c5a876d53df3c4eab1e4
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-08-24 18:56:28 +0530

    Swap values without using 3rd variable

---
+++ src/Test1.java
@@ -0,0 +1,22 @@
+
+public class Test1 {
+		public static void main(String[] args){
+			int i = 10;
+			int j = 7;
+
+		    //swapping
+			i = i + j;
+			j = i - j;
+			i = i - j;
+			
+			 System.out.println("after swapping ="+i+" "+j);
+		    System.out.println(i);
+
+		    System.out.println(j);
+
+		    // Expected Result , i = 7  and j = 10    
+		   
+		   }
+
+		}
+
