7a4eb68503fc0511ddaf2ceb7c11be617b0d7208
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-02 20:05:38 +0530

    To check output

---
+++ src/method/Test.java
@@ -0,0 +1,17 @@
+package method;
+
+ class Test {
+	protected int x, y; 
+		
+	static class Main {
+		public static void main(String args[]) {
+			Test t = new Test(); 
+
+		 System.out.println(t.x + " " + t.y); 
+
+		} 
+
+	}
+
+		}
+
