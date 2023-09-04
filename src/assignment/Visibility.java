562b42d2f3b65f116dc67116eed99d7934260985
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-04 20:30:46 +0530

    To check visibility of static variable

---
+++ src/assignment/Visibility.java
@@ -0,0 +1,10 @@
+package assignment;
+
+public class Visibility {
+		public static void main(String[] args) {
+			System.out.println(StaticMethodVisibility.add());
+			//System.out.println(StaticMethodVisibility.sub());
+			System.out.println(StaticMethodVisibility.mul());
+			System.out.println(StaticMethodVisibility.division());
+		}
+}
