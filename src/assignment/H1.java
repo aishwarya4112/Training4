ce8a5f14764f2e423ca21315b262193447d3a7b4
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-04 20:42:51 +0530

    To check visibility of instance variable

---
+++ src/assignment/H1.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class H1 {
+		public String name = "Viraj";
+		private String age = "27";
+		String country = "US";
+		protected String course = "Automation";
+		
+	public static void main(String[] args) {
+			H1 h = new H1();
+			System.out.println(h.name);
+			System.out.println(h.age);
+			System.out.println(h.course);
+			System.out.println(h.country);
+			
+	}
+
+}
---
+++ src/assignment/Visibility1.java
@@ -0,0 +1,11 @@
+package assignment;
+
+public class Visibility1 {
+		public static void main(String[] args) {
+			H1 h = new H1();
+			System.out.println(h.name);
+			System.out.println(h.course);
+			System.out.println(h.country);
+			System.out.println(h.age);
+		}
+}
