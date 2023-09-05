e71a55bebf0fcfde706b90bafa04dc110aa4b79b
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-05 10:05:33 +0530

    To check visibility of static variable

---
+++ src/assignment/S1.java
@@ -0,0 +1,12 @@
+package assignment;
+
+public class S1 {
+
+	public static void main(String[] args) {
+		System.out.println(Student.country);
+		System.out.println(Student.name);
+		System.out.println(Student.age);
+		System.out.println(Student.course);
+	}
+
+}
