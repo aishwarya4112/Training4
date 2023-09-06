a4c1acdab6329efdd6dafb2e409e6f2b397fd309
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-06 21:59:34 +0530

    To check visibility of static method

--- src/assignment/StaticMethodVisibility.java
+++ src/assignment/StaticMethodVisibility.java
@@ -8,27 +8,28 @@ public class StaticMethodVisibility {
 				return res;
 			}
 			
-			private static int sub() {
+			private static int sub() {// trying to access it in Visibility class of same package and also in Test2 class of accessmodifier package
 				int a = 30;
 				int b = 25;
 				int res = a - b;
 				return res;
 			}
 				
-			static int mul() {
+			static int mul() {// trying to access it in Visibility class of same package and also in Test2 class of accessmodifier package
 				int k = 20;
 				int l = 20;
 				int res = k *l;
 				return res;
 			}
 			
-			protected static int division() {
+			protected static int division() {// trying to access it in Visibility class of same package and also in Test2 class of accessmodifier package
 				int c = 25;
 				int d = 5;
 				int res = c / d;
 				return res;
 				  
 			}
+			
 				
 			
 			public static void main(String[] args) {
