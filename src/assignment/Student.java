65f91d75e45fe14d7717bfe9ddf5ee8921e0a4fc
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-06 20:43:40 +0530

    to check visibility of static variable

--- src/assignment/Student.java
+++ src/assignment/Student.java
@@ -2,9 +2,9 @@ package assignment;
 
 public class Student {
 	public static String country = "India";
-	private static String name = "Aishwarya";
-	static String age = "23";
-	protected static String course = "API";
+	private static String name = "Aishwarya";//i am trying to access it in S1 class in the same package and also in the accessmodifier package
+	static String age = "23";//trying to access it in S1 class of same package and also in accessmodifier package in test4 class
+	protected static String course = "java";//trying to access it in S1 class of same package and also in accessmodifier package in test4 class
 		
 		
 	public static void main(String[] args) {
