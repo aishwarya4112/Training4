f6f10d05efca5fa002a71b198b48adf0341be3f6
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-06 22:11:47 +0530

    to check visibility of non-static method

--- src/assignment/TestA.java
+++ src/assignment/TestA.java
@@ -5,15 +5,15 @@ public class TestA {
 			System.out.println("TestA -> n1(Hello)");
 		}
 		
-		private void n2() {
+		private void n2() {// trying to access it in Test3 class of accessmodifier package
 			System.out.println("TestA -> n2(Hello java)");
 		}
 		
-		void n3() {
+		void n3() {// trying to access it in Test3 class of accessmodifier package
 			System.out.println("TestA - > n3(this is java session)");
 		}
 		
-		protected void n4() {
+		protected void n4() {// trying to access it in Test3 class of accessmodifier package
 			System.out.println("TestA - > n4(Hello everyone)");
 		}
 		
