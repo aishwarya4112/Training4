eea8d4ed017cec36233b6583ffb860e8f913fed3
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-07 17:13:13 +0530

    Parameterized constructor form default constructor

---
+++ src/constructor/A.java
@@ -0,0 +1,25 @@
+package constructor;
+	class A{
+		A(){
+			System.out.println("Hello this is java constructor");
+
+		}
+			int i;
+			public A(int i){
+			i = i;
+}
+		public static void main(String[] args) {
+			A a1 = new A();
+			
+			a1.i = 411;
+			System.out.println(a1.i);
+		
+				
+			
+			
+		
+		
+			
+}
+}
+
