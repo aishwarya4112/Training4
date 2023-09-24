684a4c50cf3e120016a3cbc206d3ba2101c16a09
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-24 19:20:48 +0530

    Give values to the point class parameterized Constructor

---
+++ src/main/Point.java
@@ -0,0 +1,22 @@
+package main;
+
+	class Point { 
+		int x, y; 
+		public Point(int _x, int _y) 
+
+ { 
+				x = _x; 
+				y = _y; 
+
+		  } 
+
+		
+		public static void main(String args[]) {
+			Point p = new Point(10, 20);
+			System.out.println("x = " + p.x + ", y = " + p.y); 
+
+		  } 
+
+	}
+
+
