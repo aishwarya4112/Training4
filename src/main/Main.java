691f7b7999fda441e8a4196a13cb4c1bcf6dd85b
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-26 17:27:17 +0530

    Output of the static method

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+ 	class Main {
+	 public static void main(String args[]) {
+			System.out.println(Main.fun());
+			System.out.println(fun());
+			}
+			static int fun() {
+			return 20;
+			}
+			
+			Main m = new Main();
+ 	}
+
