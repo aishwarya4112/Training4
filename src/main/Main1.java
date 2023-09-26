bed51ab49a6027085506337cf2d1d2295a930556
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-26 17:57:50 +0530

    Assignment

---
+++ src/main/Main1.java
@@ -0,0 +1,14 @@
+package main;
+
+	class Main1 {
+		
+			public static void main(String args[]) {
+			System.out.println(Main1.fun());
+			}
+			static int fun() {
+			return 20;
+			}
+			
+		 Main1 m1 = new Main1();
+			}
+
