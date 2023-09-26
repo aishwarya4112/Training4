3da7f18ab271142169383c83455541055747510b
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-26 17:39:24 +0530

    Outpun of the program

---
+++ src/main/Test.java
@@ -0,0 +1,22 @@
+package main;
+
+ class Test {
+	 
+		 private static int x;
+		 public static void main(String args[]) {
+		 //System.out.println(fun());
+		 System.out.println(Test.x);
+		 System.out.println(Test.fun());
+		 }
+		 static int fun() {
+		 return ++x;
+		 }
+		 
+		 Test t = new Test();
+		 
+		 }
+	
+
+	
+
+
