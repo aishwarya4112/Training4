cb578615d04845b4dd2417cd2adf4f924ead83fa
Aishwarya <Aishwarya@LAPTOP-OF02TNRN>
2023-09-30 18:46:51 +0530

    Variable notation

---
+++ src/method/VariableNotation.java
@@ -0,0 +1,16 @@
+package method;
+
+public class VariableNotation {
+		static int i = 10; 
+		//int int = 20; //int is reserved keyword
+		//int goto = 30; // goto is reserved keyword
+		//int default = 40; // default is reserved keyword
+		//int class = 50; // class is reserved keyword
+		 static int j = 70;
+	public static void main(String[] args) {
+		System.out.println(i);
+		System.out.println(j);
+				 
+}
+
+}
